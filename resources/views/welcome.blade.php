b86e67b0a4deb993ca17c32c320507e2b0d7d5dc
joe <kongrath.jojo@gmail.com>
2015-02-21 00:11:06 +0700

    update message in welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <html>
 	<head>
+		<title>CSE Biz</title>
+
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
 		<style>
@@ -38,8 +40,8 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<div class="title">We are comming soon.</div>
+				<!-- <div class="quote">{{ Inspiring::quote() }}</div> -->
 			</div>
 		</div>
 	</body>
